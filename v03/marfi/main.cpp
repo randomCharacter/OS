bc047814f791c8cff4e399f984c962f8f5c568f9
Mario Perić <pericmario96@gmail.com>
2017-04-02 00:05:39 +0200

    v03

---
+++ v03/marfi/main.cpp
@@ -0,0 +1,88 @@
+/*
+    Napraviti program koji ispituje ispravnost jednog od Marfijevih zakona, koji glasi:
+    "Vas red je uvek najsporiji.".
+
+    Simulirati placanje robe na tri kase. Rad svake kase predstavljen je jednom niti.
+    Svaka nit dobija parametar koji predstavlja broj kupaca koje kasa treba da opsluzi.
+    Broj kupaca je slucajan broj izmedju 1 i 30.
+    Opsluzivanje svakog kupca simulirati uspavljivanjem niti na slucajan vremenski period izmedju 1 i 100 ms.
+
+    Izracunati trajanje kupovine na svakoj od kasa.
+
+    Na pocetku programa potrebno je pitati korisnika da unese redni broj kase na
+    kojoj zeli da izvrsi kupovinu.
+    Na kraju ispisati da li je Marfijev zakon potvrdjen.
+    Ukoliko je kupovina na kasi koju je kupac izabrao trajala najduze, tada je Marfijev zakon potvrdjen.
+*/
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+using namespace chrono;
+
+// Broj kasa
+const int N = 3;
+// Maksimalno vreme cekanja
+const int MAX_CEKANJE = 100;
+// Maksimalan broj ljudi u redu
+const int MAX_LJUDI = 30;
+
+// Funkcija koja simulira cekanje u redu sa n kupaca
+void f(int n, duration<double, milli> &v) {
+    // Uzimanje vremena pre početka izvršavanja
+    auto t0 = system_clock::now();
+
+    // Simuliranje čekanja
+    for (int i = 0; i < n; i++) {
+        // Uspavljuje trenutnu nit za prosledjeno vreme
+        this_thread::sleep_for(milliseconds(rand() % MAX_CEKANJE + 1));
+    }
+
+    // Uzimanje vremena nakon završetka izvršavanja
+    auto t1 = system_clock::now();
+
+    // Ukupno čekanje
+    v = t1 - t0;
+}
+
+int main() {
+    // Slucajni brojevi se racunaju u odnosu na trnutno vreme
+    srand(time(NULL));
+
+    // Redni broj kase
+    int n;
+    cout << "Unesite redni broj kase [0<=n<3]: " << endl;
+    cin >> n;
+
+    // Vremena čekanja na svakoj kasi
+    duration<double, milli> v[N];
+
+    // Niti za simulaciju čekanja
+    thread t[N];
+
+    //Simulacija čekanja
+    for (int i = 0; i < N; i++) {
+        t[i] = thread(f, rand() & MAX_LJUDI + 1, ref(v[i]));
+    }
+    for (int i = 0; i < N; i++) {
+        t[i].join();
+    }
+
+    // Pronalaženje reda u kojem se najduže čekalo
+    auto max = v[0];
+    for (int i = 0; i < N; i++) {
+        cout << "Ukupno cekanje reda br. " << i << " je: " << v[i].count() << endl;
+        // Pronalaženje maksimuma
+        if (v[i] > max) {
+            max = v[i];
+        }
+    }
+
+    // Provera zakona
+    if (v[n] == max) {
+        cout << "Marfijev zakon potvrdjen!" << endl;
+    } else {
+        cout << "Marfijev zakon nije potvrdjen!" << endl;
+    }
+}
---
+++ v03/najblizi_nuli_vreme_prosirenje/main.cpp
@@ -0,0 +1,85 @@
+/*
+    Prosiriti zadatak najbilzi_nuli_vreme.cpp ispisom u sekundama i minutima.
+*/
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <cmath>
+
+using namespace std;
+using namespace chrono;
+
+// Broj elemenata niza
+const int N = 900000;
+// Broj niti
+const int BROJ_NITI = 3;
+
+// Funkcija koja traži minimum
+void f(vector<double>::const_iterator pocetak, vector<double>::const_iterator kraj, double &min, duration<double, milli> &v) {
+    // Uzimanje vremena pre početka izvršavanja
+    system_clock::time_point t0 = system_clock::now();
+    // Traženje minimuma
+    min = *pocetak;
+    for (auto it = pocetak + 1; it != kraj; it++) {
+        if (abs(*it) < abs(min)) {
+            min = *it;
+        }
+    }
+    // Uzimanje vremena nakon završetka izvršavanja
+    system_clock::time_point t1 = system_clock::now();
+
+    // Ukupno trajanje je razlika vremena kraja i početka
+    v = t1 - t0;
+}
+
+int main() {
+    // Slucajni brojevi se racunaju u odnosu na trnutno vreme
+    srand(time(NULL));
+
+    vector<double> A(N);
+
+    // Popunjavanje vektora slučajnim brojevima
+    for (int i = 0; i < N; i++) {
+        A[i] = rand();
+    }
+
+    // Potrebne niti
+    thread t[BROJ_NITI];
+    // Traženi minimumi
+    double m[BROJ_NITI];
+    // Vremena izvršavanja
+    duration<double, milli> v[BROJ_NITI];
+
+    // Pokretanje niti
+    for (int i = 0; i < BROJ_NITI; i++) {
+        t[i] = thread(f, A.begin() + A.size() * i / BROJ_NITI, A.begin() + A.size() * (i+1) / BROJ_NITI, ref(m[i]), ref(v[i]));
+    }
+
+    // Čekanje da se niti završe
+    for (int i = 0; i < BROJ_NITI; i++) {
+        t[i].join();
+    }
+
+    // Traženje minimuma dobijenih minimuma
+    double min = m[0];
+    for (int i = 1; i < BROJ_NITI; i++) {
+        if (abs(m[i]) < abs(min)) {
+            min = m[i];
+        }
+    }
+
+    //Ispis rezultata
+    cout << "Element najblizi nuli: " << min << endl;
+
+    // Ispis vremena trajanja
+    for (int i = 0; i < BROJ_NITI; i++) {
+        duration<double, ratio<60>> mins= v[i];
+        duration<double> secs= v[i];
+        duration<double, milli> millis= v[i];
+        cout << "Nit br. " << i << " se izvrsavala " << millis.count() << " milisekundi." << endl;
+        cout << "Nit br. " << i << " se izvrsavala " << secs.count() << " sekundi." << endl;
+        cout << "Nit br. " << i << " se izvrsavala " << mins.count() << " minuta." << endl;
+        cout << endl;
+    }
+}
